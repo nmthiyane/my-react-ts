4cf6989ffac1227b54cda0d050debe3fb605415d
Ntuthuko Mthiyane <ntuthuko.mthiyane@SAMAC001.local>
2018-03-06 15:32:44 +0200

    added test for the user position function

--- src/utils/race.spec.ts
+++ src/utils/race.spec.ts
@@ -1,4 +1,20 @@
 /*
-	Functions that should have tests from "./race.ts"
-	- calculateRacerPosition
+ Author: Ntuthuko Mthiyane
+ Date: 06/03/2018
+ Description: This test is responsible for testing functions in race.js
 */
+
+import { calculateRacerPosition } from './race';
+
+describe('it tests the calculate racer position function in race', () => {
+
+	let currentPosition: number = 0;
+	let expected: number;
+	it('It checks that the position of racer increases everytime', () => {
+		while (currentPosition <= 100) {
+			expected = calculateRacerPosition(currentPosition);
+			expect(expected).toBeGreaterThan(currentPosition);
+			currentPosition = expected;
+		}
+	});
+});
