fcf33afc515706192182cae29b05d91c733ac087
Ntuthuko Mthiyane <ntuthuko.mthiyane@SAMAC001.local>
2018-03-06 11:40:02 +0200

    added test for data spec

--- src/utils/data.spec.ts
+++ src/utils/data.spec.ts
@@ -1,4 +1,34 @@
 /*
-	Functions that should have tests from "./data.ts"
+	Functions that should have tests from './data.ts'
 	- getRacerData
 */
+import { getRacerData } from './data';
+
+describe('It checks if data is returned from from array', () => {
+	/* This will iterate through the returned array and check that the objects 
+		returned all have the required properties and are not null */
+	it('It checks if returned objects all conatin properties', () => {
+		getRacerData().map((actual) => {
+			expect(actual).toMatchObject(
+				expect.objectContaining({
+					login: expect.any(String),
+					id: expect.any(Number),
+					avatar_url: expect.any(String),
+					gravatar_id: expect.any(String),
+					url: expect.any(String),
+					html_url: expect.any(String),
+					followers_url: expect.any(String),
+					gists_url: expect.any(String),
+					starred_url: expect.any(String),
+					subscriptions_url: expect.any(String),
+					organizations_url: expect.any(String),
+					repos_url: expect.any(String),
+					events_url: expect.any(String),
+					received_events_url: expect.any(String),
+					type: expect.any(String),
+					site_admin: expect.any(Boolean),
+				})
+			);
+		});
+	});
+});
