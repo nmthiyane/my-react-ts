1d7f4792a5956c19c0c1b0647bfbc327426a01a9
Ntuthuko Mthiyane <ntuthuko.mthiyane@SAMAC001.local>
2018-03-06 17:33:04 +0200

    mixed header test

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,3 +1,17 @@
-/*
-	Snapshot tests for "./Header.tsx"
-*/
+import * as React  from 'react';
+import { Header } from './Header';
+const renderer  = require('react-test-renderer');
+
+describe('Snapshot test for the entire app', () => {
+	let lastWinner: string =  'nmthiyane';
+	let raceInProgress: boolean = false;
+	it('renders a snapshot', () => {
+	const tree = renderer.create((
+		<Header 
+			lastWinner={lastWinner} 
+			raceInProgress={raceInProgress}
+		/>
+		)).toJSON();
+	expect(tree).toMatchSnapshot();
+	});
+});
--- src/components/Track/Track.spec.tsx
+++ src/components/Track/Track.spec.tsx
@@ -1,3 +1,10 @@
-/*
-	Snapshot tests for "./Track.tsx"
-*/
+import * as React  from 'react';
+import { Track } from './Track';
+const renderer  = require('react-test-renderer');
+
+describe('Snapshot test for the entire app', () => {
+	it('renders a snapshot', () => {
+	const tree = renderer.create((<Track />)).toJSON();
+	expect(tree).toMatchSnapshot();
+	});
+});
